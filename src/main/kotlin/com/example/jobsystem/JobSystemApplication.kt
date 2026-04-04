d401d565272d7fcb43044414bbf0dd35dc439763
P Jotsna Sai <jotsnasai33@gmail.com>
2026-04-04 12:09:59 +0530

    docker setup

--- src/main/kotlin/com/example/jobsystem/JobSystemApplication.kt
+++ src/main/kotlin/com/example/jobsystem/JobSystemApplication.kt
@@ -3,11 +3,13 @@ package com.example.jobsystem
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.scheduling.annotation.EnableAsync
+import java.util.TimeZone
 
 @EnableAsync
 @SpringBootApplication
 class JobSystemApplication
 
 fun main(args: Array<String>) {
+	TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
 	runApplication<JobSystemApplication>(*args)
 }
